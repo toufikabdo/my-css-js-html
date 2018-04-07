e100b47cc1f838fcfe3ad6b82902d95abfcb2f2e
toufikabdo <toufik.bid2@gmail.com>
2018-04-07 04:44:32 +0200

    Add files via upload

---
+++ P-PU/pushfox.js
@@ -0,0 +1,15 @@
+//** push subscribers **//
+// https://www.foxpush.com/ 
+
+<script type="text/javascript" data-cfasync="false">
+        var _foxpush = _foxpush || [];
+        _foxpush.push(['_setDomain', 'profeuserblogspotcom']);
+
+        (function(){
+            var foxscript = document.createElement('script');
+            foxscript.src = '//cdn.foxpush.net/sdk/foxpush_SDK_min.js';
+            foxscript.type = 'text/javascript';
+            foxscript.async = 'true';
+            var fox_s = document.getElementsByTagName('script')[0];
+            fox_s.parentNode.insertBefore(foxscript, fox_s);})();
+    </script>
